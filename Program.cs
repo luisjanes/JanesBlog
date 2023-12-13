eb96973e951406e4a7950806167c512067c2af4e
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-12-13 18:55:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System.Runtime.CompilerServices;
+using Dapper;
+using JanesBlog.Models;
+using Dapper.Contrib.Extensions;
+using JanesBlog.Repositories;
+
+public class Program
+{
+    private const string CONNECTION_STRING = @"Server=localhost;Database=Blog;User ID=root; Password=123456";
+    public static void Main(string[] args)
+    {
+        var connection = new MySqlConnection(CONNECTION_STRING);
+        ReadUsers(connection);
+        //ReadUser(connection);
+        //CreateUser(connection);
+        //UpdateUser(connection);
+        //DeleteUser(connection);
+    }
+    public static void ReadUsers(MySqlConnection connection)
+    {
+        var userRepository = new UserRepository(connection);
+        var users = userRepository.Get();
+        foreach (var user in users)
+            Console.WriteLine(user.Name);
+
+    }
+    public static void ReadUser(MySqlConnection connection)
+    {
+        var user = connection.Get<User>(2);
+        Console.WriteLine(user.Name);
+        Console.WriteLine(user.Email);
+    }
+    public static void CreateUser(MySqlConnection connection)
+    {
+        var user = new User() 
+        { 
+            Id = 0,
+            Name = "Carolina",
+            Email = "carolina@gmail.com",
+            PasswordHash = "HASH",
+            Bio = "A Grande Carolina",
+            Image = "https://",
+            Slug = "carolina"
+        };
+        connection.Insert<User>(user);
+    }
+    public static void UpdateUser(MySqlConnection connection)
+    {
+        var user = new User()
+        {
+            Id = 2,
+            Name = "Carolina",
+            Email = "carolina2@gmail.com",
+            PasswordHash = "HASH",
+            Bio = "A Grande Carolina",
+            Image = "https://",
+            Slug = "carolina2"
+        };
+        connection.Update<User>(user);
+    }
+    public static void DeleteUser(MySqlConnection connection)
+    {
+        var user = connection.Get<User>(6);
+        connection.Delete<User>(user);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper.Contrib.Extensions;
+using JanesBlog.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanesBlog.Repositories
+{
+    public class UserRepository(MySqlConnection connection)
+    {
+        private readonly MySqlConnection _connection = connection;
+
+        public IEnumerable<User> Get()
+            => _connection.GetAll<User>();
+
+        public User Get(int id)
+            => _connection.Get<User>(id);
+
+        public void Create(User user)
+            => _connection.Insert<User>(user);
+    }
+}
